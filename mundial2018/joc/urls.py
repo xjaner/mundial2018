8dd3addfa9d75c373ae5b7293ab01d699f0e0e6c
Xavier Janer <xavier.janer@qustodio.com>
2018-04-12 00:00:11 +0200

    Acabada la pantalla de login

--- mundial2018/joc/urls.py
+++ mundial2018/joc/urls.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 # from django.urls import include, path
+from django.contrib.auth.decorators import login_required
 
 from . import views
-# from views.usuaris import UsuarisView
-# from views.consulta import ConsultaView
+from .views.usuaris import UsuarisView
+from .views.consulta import ConsultaView
 # from views.classificacio import ClassificacioView
 
 app_name = 'joc'
 urlpatterns = [
     # Comunes
     path('', views.index, name='index'),
-    # url(r'^consulta_grups$', views.consulta_grups, name='consulta_grups'),
-    # url(r'^consulta$', login_required(ConsultaView.as_view()), name='consulta'),
-    # url(r'^usuaris$', login_required(UsuarisView.as_view()), name='usuaris'),
+    path('consulta_grups', views.consulta_grups, name='consulta_grups'),
+    path('consulta', login_required(ConsultaView.as_view()), name='consulta'),
+    path('usuaris', login_required(UsuarisView.as_view()), name='usuaris'),
     # url(r'^entrada_admin$', views.entrada_admin, name='entrada_admin'),
     # url(r'^pronostic_admin$', views.pronostic_admin, name='pronostic'),
     # url(r'^puntuacions$', views.puntuacions, name='puntuacions'),
     # # Només abans
-    # # url(r'^pronostic$', views.pronostic, name='pronostic'),
+    path('pronostic', views.pronostic, name='pronostic'),
     # # Només després
     # url(r'^classificacio$', login_required(ClassificacioView.as_view()), name='classificacio'),
 ]
