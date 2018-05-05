bca3f101fb1396b85d5357bf03b66be09fd86992
Xavier Janer <xavier.janer@qustodio.com>
2018-05-06 00:20:34 +0200

    Desfet els canvis a l'index

--- mundial2018/joc/urls.py
+++ mundial2018/joc/urls.py
@@ -6,7 +6,9 @@ from django.contrib.auth.decorators import login_required
 from . import views
 from .views.usuaris import UsuarisView
 from .views.consulta import ConsultaView
-from .views.classificacio import ClassificacioView
+
+# Només després
+# from .views.classificacio import ClassificacioView
 
 app_name = 'joc'
 urlpatterns = [
@@ -15,11 +17,11 @@ urlpatterns = [
     path('consulta_grups', views.consulta_grups, name='consulta_grups'),
     path('consulta', login_required(ConsultaView.as_view()), name='consulta'),
     path('usuaris', login_required(UsuarisView.as_view()), name='usuaris'),
-    # url(r'^entrada_admin$', views.entrada_admin, name='entrada_admin'),
-    # url(r'^pronostic_admin$', views.pronostic_admin, name='pronostic'),
     path(r'^puntuacions$', views.puntuacions, name='puntuacions'),
     # # Només abans
-    # path('pronostic', views.pronostic, name='pronostic'),
+    path('pronostic', views.pronostic, name='pronostic'),
     # # Només després
-    path(r'^classificacio$', login_required(ClassificacioView.as_view()), name='classificacio'),
+    # path(r'^classificacio$', login_required(ClassificacioView.as_view()), name='classificacio'),
+    # url(r'^entrada_admin$', views.entrada_admin, name='entrada_admin'),
+    # url(r'^pronostic_admin$', views.pronostic_admin, name='pronostic'),
 ]
