8a41de2bd2f0be2753728085ef71efcc2e5f3bc6
Xavier Janer <xavier.janer@qustodio.com>
2018-05-26 20:25:39 +0200

    Mostrar el nom d'usuari al llistat de Jugadors a l'admin

--- mundial2018/joc/admin.py
+++ mundial2018/joc/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 
 from .models import Equip, Grup, Jugador, Partit
 
+
 class PartitAdmin(admin.ModelAdmin):
     readonly_fields = ('id', )
 
+
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ('usuari__username')
+
 admin.site.register(Equip)
 admin.site.register(Grup)
-admin.site.register(Jugador)
+admin.site.register(Jugador, JugadorAdmin)
 admin.site.register(Partit, PartitAdmin)
